bc0fdbe152b90be325bd64fdd983046355154d1e
Nishchal Visavadiya <nishchal.vn@gmail.com>
2024-10-13 15:26:18 +0530

    Initial commit

---
+++ SwiftConcurrencyDemo/SwiftConcurrencyDemo/SwiftConcurrencyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyDemoApp.swift
+//  SwiftConcurrencyDemo
+//
+//  Created by Nishchal Visavadiya on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
